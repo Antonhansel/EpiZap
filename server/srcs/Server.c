3eab66703b87b06c7c2dc505b260b79b3c0e7d90
david_c <flavien.david@epitech.eu>
2014-06-24 18:19:06 +0200

    adding saving of player

--- server/srcs/Server.c
+++ server/srcs/Server.c
@@ -33,7 +33,7 @@ static int	accept_socket(Server *s)
 	printf("--- LISTEN ON PORT %d ---\n", s->port);
 	int len = sizeof(s->sin);
 	int fd = xaccept(s->socket, &(s->sin), (socklen_t *)&len);
-	/*if (s->clients == NULL)
+	if (s->clients == NULL)
 	{
 		s->clients = malloc(sizeof(void) * 100);
 		s->clients = memset(s->clients, 0, 100);
@@ -42,12 +42,11 @@ static int	accept_socket(Server *s)
 	if ((c = malloc(sizeof(Client))) == NULL)
 		return (-1);
 	createClient(c, fd);
-	s->clients[fd] = c;*/
-	//s->maxFd = fd;
+	s->clients[fd] = c;
+	if (s->maxFd < fd)
+		s->maxFd = fd;
 	printf("--- CLIENT ACCEPT ON FD : %d ---\n", fd);
-/*	if (write(s->socket, "TT\n", 3) <= 0)
-		printf("----> ERROR ON WRITE ---\n");
-*/	if (write(fd, "--- SUCCESSLY CONNECT ---\n", 26) <= 0)
+	if (write(fd, "--- SUCCESSLY CONNECT ---\n", 26) <= 0)
 		return (1);
 	return (0);
 }
@@ -56,46 +55,31 @@ int 				serverLoop(Server *this)
 {
 	int 			error;
 	int 			result;
-	//int 			resultPrev;
+	int 			resultPrev;
 	fd_set 			readfds;
-	//fd_set 			writefds;
 	struct timeval	tv;
 
 	tv.tv_usec = 100;
 	tv.tv_sec = 0;
 	error = 0;
 	result = 0;
-	//resultPrev = 0;
+	resultPrev = 0;
 	printf("ADDR SERVER : %p && SOCKET FD = %d\n", this, this->socket);
 	while (!error)
 	{
 		FD_ZERO(&readfds);
 		FD_SET(this->socket, &readfds);
-		//(*this->init_fd)(this, &readfds);
 		if ((result = select(this->maxFd + 1, &readfds, NULL, NULL, &tv)) == -1)
 			return (1);
 		else if (FD_ISSET(this->socket, &readfds))
 		{
-			printf("SOCKET\n");
 			(*this->accept_socket)(this);
-			/*int i;
-			for (i = 0; i < 10; i++)
-			{
-				if (this->clients[i] != NULL)
-				{
-					printf("--- CLIENT ON FD %d WITH REAL FD %d---\n", i, this->clients[i]->fd);
-				}
-			}*/
 		}
-		/*else if (result != resultPrev && this->clients != NULL && FD_ISSET(4, &writefds))
-		{
-			printf("--- CHECK ---\n");
-		}*/
-		/*else if (result != resultPrev)
+		else if (result != resultPrev)
 		{
 			(*this->check_fd)(this, &readfds);
 			resultPrev = result;
-		}*/
+		}
 	}
 	return (0);
 }
