91022e8034cff8beea422626d47bc6f03fd9cf68
david_c <flavien.david@epitech.eu>
2014-06-25 11:58:55 +0200

    clenaing code

--- server/srcs/Player.c
+++ server/srcs/Player.c
@@ -1,11 +1,1 @@
-/*
-** Client.c for Client.c in /home/david_c/B4/systemUnix/psu_2013_zappy/server/srcs
-**
-** Made by jeremie taboada-alvarez
-** Login   <david_c@epitech.net>
-**
-** Started on  mer. juin  18 16:42:27 2014 jeremie taboada-alvarez
-** Last update mer. juin  18 16:42:27 2014 jeremie taboada-alvarez
-*/
-
 #include "Player.h"
