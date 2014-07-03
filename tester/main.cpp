c52abcb7604f0d81e4f6d96d50fc151af67e0a6c
Antonin Ribeaud <antoninribeaud@gmail.com>
2014-07-03 16:04:07 +0200

    Adding usage for tester

--- tester/main.cpp
+++ tester/main.cpp
@@ -7,7 +7,16 @@
 
 int		main(int argc, char **argv)
 {
-  QApplication	app(argc, argv);
-  MainUI	Client(argv[1], argv[2], argv[3]);
-  return (app.exec());
+	if (argc == 4)
+	{
+		QApplication	app(argc, argv);
+
+		MainUI	Client(argv[1], argv[2], argv[3]);
+		return (app.exec());
+	}
+	else
+	{
+		std::cout << "./tester [ip] [port] [team to join]" << std::endl;
+		return 1;
+	}
 }
