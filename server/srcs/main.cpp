1cd090a7a82b5e5786d222655f8f89af433f4502
Mehdi Chouag <mehdi.chouag@epitech.eu>
2014-07-05 18:58:07 +0200

    resize the window

--- server/srcs/main.cpp
+++ server/srcs/main.cpp
@@ -25,7 +25,7 @@ int		main(int argc, char **argv)
   int mh = size.height();
   int centerW = (width/2) - (mw/2);
   int centerH = (height/2) - (mh/2);
-  Client.setFixedSize(1764, 878);
+  Client.setFixedSize(1725, 878);
   Client.move(centerW, centerH);
   return (app.exec());
 }
